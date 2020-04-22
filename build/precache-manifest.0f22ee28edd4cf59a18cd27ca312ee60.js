529bd306b04d2918c73f1d623b96191933e2f609
wataru haraguchi <wharaguchi23@gmail.com>
2020-04-22 17:54:38 +0900

    build

---
+++ build/precache-manifest.0f22ee28edd4cf59a18cd27ca312ee60.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "893d728bc34d7074b45f",
+    "url": "/static/js/main.ad013728.chunk.js"
+  },
+  {
+    "revision": "65a92b334fee1009911e",
+    "url": "/static/js/2.28800bff.chunk.js"
+  },
+  {
+    "revision": "893d728bc34d7074b45f",
+    "url": "/static/css/main.eed4f069.chunk.css"
+  },
+  {
+    "revision": "2f211205f70931d7785288fdb8c0ac6f",
+    "url": "/index.html"
+  }
+];
