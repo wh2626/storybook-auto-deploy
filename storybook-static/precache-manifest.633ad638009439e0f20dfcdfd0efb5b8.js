8b5f3447bd4e151f503ba5e701be31744d3456ce
wataru haraguchi <wharaguchi23@gmail.com>
2020-05-20 12:16:58 +0900

    build

---
+++ storybook-static/precache-manifest.633ad638009439e0f20dfcdfd0efb5b8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "url": "vendors~main.8eaf5ecfe64064141048.bundle.js.LICENSE.txt"
+  },
+  {
+    "url": "vendors~main.8eaf5ecfe64064141048.bundle.js"
+  },
+  {
+    "revision": "83ca5f04c22d4ff0dfc5",
+    "url": "static/css/main.e01ea36c.chunk.css"
+  },
+  {
+    "url": "runtime~main.8eaf5ecfe64064141048.bundle.js"
+  },
+  {
+    "url": "main.8eaf5ecfe64064141048.bundle.js"
+  },
+  {
+    "revision": "ef0375a15965e788191e31c7e5db42a3",
+    "url": "iframe.html"
+  }
+];
